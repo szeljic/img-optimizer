2d8e8f2f29d0e1ce34f0a4a36fe9503204f9b3cc
zeljic <zeljic@gmail.com>
2014-03-26 19:48:30 +0100

    Working on design - css file is added - two icons are added

--- src/main/java/com/zeljic/pngoptimizer/controllers/BootController.java
+++ src/main/java/com/zeljic/pngoptimizer/controllers/BootController.java
@@ -3,23 +3,27 @@ package com.zeljic.pngoptimizer.controllers;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import com.zeljic.pngoptimizer.imageitem.Item;
-
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
-import javafx.scene.control.RadioButton;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TitledPane;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
+
+import com.zeljic.pngoptimizer.imageitem.Item;
 
 public class BootController implements Initializable
 {
 	
 	@FXML
-	private RadioButton rbTypeFiles, rbTypeDirectory;
+	private ToggleButton btnFiles, btnDirectory;
+	
+	@FXML
+	private ToggleGroup tgType;
 	
 	@FXML
 	private TextField txtLocation;
